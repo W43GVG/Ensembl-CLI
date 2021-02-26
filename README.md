c48d3bb72ef242e220316303ff53d093d0b8ce09
W43GVG <worldfirenine@gmail.com>
2021-02-26 10:43:08 +0100

    Ontologies

--- README.md
+++ README.md
@@ -16,11 +16,32 @@
  python ensembl-cli.py COMMANDS
  ```
 
+ #### Get help
+ ```
+ python ensembl-cli.py COMMAND --help
+ ```
+
+ #### Ontologies
+ Multiple ontologies can be obtained ([gene ontology](http://www.geneontology.org/): `GO`, [sequence ontology](http://www.sequenceontology.org/): `SO`, [protein ontology](https://proconsortium.org/): `PR`, [uberon](https://uberon.github.io/): `UBERON`). 
+
+ ```
+ python ensembl-cli.py onto_id 0000370 SO json
+ ```
+
+ ![First lines of the output](https://i.imgur.com/SjbVHOi.png)
+
+ Search ontological terms:
+
+ ```
+ python ensembl-cli.py onto_name TERM FORMAT
+ ```
+
  Example:
+
  ```
- python ensemble-cli.py tax_classif 7819 json
+ python ensembl-cli.py onto_name tubulin json
  ```
- 
+
  ## License, notes
  W43GVG under the MIT License.
 
--- ensembl-cli.py
+++ ensembl-cli.py
@@ -2,6 +2,7 @@ import sys, requests, fire, json, xml
 
 server = "https://rest.ensembl.org"
 exp_message = "\nExport output?\nIf Yes, it will overwrite output.(json/xml)\nY or N\n"
+json_search_message = "Do you want to search in the json?\nY or N\n"
 
 def GET_tax_id(id, format):
     """
@@ -36,7 +37,7 @@ def GET_tax_id(id, format):
 def GET_tax_name(name, format):
     """
     Search for a taxonomic id by a non-scientific name
-        | name: A non-scientific species name. Can include SQL wildcards
+        | name: a non-scientific species name
         | format: either JSON or XML
     """
     ext = "/taxonomy/name/"
@@ -67,7 +68,7 @@ def GET_tax_name(name, format):
 def GET_tax_classification(id, format):
     """
     Return the taxonomic classification of a taxon node
-        | id: A taxon identifier. Can be a NCBI taxon id or a name
+        | id: a taxon identifier. Can be a NCBI taxon id or a name
         | format: either JSON or XML
     """
     ext = "/taxonomy/classification/"
@@ -94,9 +95,72 @@ def GET_tax_classification(id, format):
             with open("output.xml", "w") as xml_f:
                 xml_f.write(r.text)
 
+def GET_onto_id (id, ontology, format):
+    """
+    Search for an ontological term by its identifier (digits)
+        | id: ontology term identifier
+        | ontology: ontology (see the README)
+        | format: either JSON or XML
+    """
+    ext = "/ontology/id/" + str(ontology) + ":"
+    exp = None
+    url = server + ext + str(id) + "?"
+    
+    if format == "json":
+        r = requests.get(url, headers = {"Content-Type" : "application/json"})
+
+        data = json.loads(r.text)
+        print(json.dumps(data, indent = 2))
+        
+        exp = input(exp_message)
+        if exp == "Y":
+            with open("output.json", "w") as json_f:
+                json.dump(data, json_f, indent=2)
+    elif format == "xml":
+        r = requests.get(url, headers = {"Content-Type" : "application/xml"})
+
+        print(r.text)
+
+        exp = input(exp_message)
+        if exp == "Y":
+            with open("output.xml", "w") as xml_f:
+                xml_f.write(r.text)
+
+def GET_onto_name (name, format):
+    """
+    Search for a list of ontological terms by their name
+        | name: an ontology name
+        | format: either JSON or XML
+    """
+    ext = "/ontology/name/"
+    exp = None
+    url = server + ext + str(name) + "?"
+    
+    if format == "json":
+        r = requests.get(url, headers = {"Content-Type" : "application/json"})
+
+        data = json.loads(r.text)
+        print(json.dumps(data, indent = 2))
+        
+        exp = input(exp_message)
+        if exp == "Y":
+            with open("output.json", "w") as json_f:
+                json.dump(data, json_f, indent=2)
+    elif format == "xml":
+        r = requests.get(url, headers = {"Content-Type" : "application/xml"})
+
+        print(r.text)
+
+        exp = input(exp_message)
+        if exp == "Y":
+            with open("output.xml", "w") as xml_f:
+                xml_f.write(r.text)
+
 if __name__ == "__main__":
     fire.Fire({
         "tax_id" : GET_tax_id,
         "tax_name" : GET_tax_name,
-        "tax_classif" : GET_tax_classification
+        "tax_classif" : GET_tax_classification,
+        "onto_id" : GET_onto_id,
+        "onto_name" : GET_onto_name
     })
